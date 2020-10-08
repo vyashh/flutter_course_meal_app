77f63f711b13c551db929389c818d1e971109c0d
unknown <vyashbhawan@gmail.com>
2020-10-08 20:20:57 +0200

    added meal data with image

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../dummy_data.dart';
+import '../widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList(); // filter alle meal op basis vcn de category id
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
+            duration: categoryMeals[index].duration,
+          );
+        },
+        itemCount:
+            categoryMeals.length, // geef aan hoeveel items je wilt genereren
+      ), // ListView.builder gebruiken voor betere performance. Haalt items die niet in je scherm staan weg.
+    );
+  }
+}
